d658acf73305edf23d830d7cbd58318e885f79c0
Simeon-Tchameu <emsitchameu@gmail.com>
2024-07-06 12:32:22 -0400

    Add styles

---
+++ js/app.js
@@ -0,0 +1,17 @@
+document.addEventListener("scroll", function () {
+  const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  if (window.scrollY > 300) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+});
+
+document
+  .getElementById("scrollToTopBtn")
+  .addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
